9b2e1c2f53b53225bfb8e82d38a71c6d95a815b0
fordecember <1921567337@qq.com>
2025-04-02 10:58:05 +0800

    init

---
+++ src/main/java/com/whustudy/cengkehelper/CengkeHelperApplication.java
@@ -0,0 +1,13 @@
+package com.whustudy.cengkehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CengkeHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CengkeHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whustudy/cengkehelper/CengkeHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whustudy.cengkehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CengkeHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
